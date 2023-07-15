1e870323e560f08afae4c0168c8811153a649492
cwhite70 <chase.t.white@hotmail.com>
2023-07-14 18:30:17 -0600

    update

--- pages/api/drinks/addFavorite.ts
+++ pages/api/drinks/addFavorite.ts
@@ -1,8 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
+import { Prisma } from "@prisma/client";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const session = await getServerSession(req, res, authOptions);
   if (!session) {
     return res
@@ -10,10 +15,12 @@ export default async function handler(req, res) {
       .json({ message: "Please sign in to add drink to favorites" });
   }
 
+  const email = session.user?.email as string;
+
   // Get User
   const prismaUser = await prisma.user.findUnique({
     where: {
-      email: session.user.email,
+      email: email,
     },
   });
 
@@ -21,19 +28,24 @@ export default async function handler(req, res) {
   const favorite = await prisma.favorite.findFirst({
     where: {
       drinkId: req.body.drinkId,
-      userId: prismaUser.id,
+      userId: prismaUser?.id,
     },
   });
 
   if (req.method === "POST") {
     try {
       if (!favorite) {
-        const result = await prisma.favorite.create({
-          data: {
-            drinkId: req.body.drinkId,
-            userId: prismaUser.id,
-          },
-        });
+        const data: Prisma.FavoriteCreateInput = {
+          drink: req.body.drinkId,
+          user: prismaUser?.id
+            ? { connect: { id: prismaUser.id } }
+            : {
+                create: {
+                  /* user creation data */
+                },
+              },
+        };
+        const result = await prisma.favorite.create({ data });
         res.status(201).json(result);
       } else {
         const result = await prisma.favorite.delete({
