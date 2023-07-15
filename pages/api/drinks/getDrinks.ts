83406e5697dd35d882737a6831bc345aef418555
cwhite70 <chase.t.white@hotmail.com>
2023-07-14 18:35:09 -0600

    update

--- pages/api/drinks/getDrinks.ts
+++ pages/api/drinks/getDrinks.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const fetchDrinks = async ({ searchCocktail, searchingFor }) => {
+const fetchDrinks = async ({ searchCocktail, searchingFor }: any) => {
   const url = "www.thecocktaildb.com/api/json/v1/1/";
   const type = searchCocktail ? "search.php?s=" : "filter.php?i=";
 
   const res = await axios.get(`${url}${type}${searchingFor}`);
-  console.log(res.data);
 
   return res.data;
 };
