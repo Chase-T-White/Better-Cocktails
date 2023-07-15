50a34fb61e675c2fa9ae623540222137445a19d3
cwhite70 <chase.t.white@hotmail.com>
2023-07-14 18:00:24 -0600

    update

--- app/components/Ingredients.tsx
+++ app/components/Ingredients.tsx
@@ -24,19 +24,22 @@ const Ingredients: React.FC<IngredientsProps> = ({ setSearchingFor }) => {
 
   if (!data || !data.drinks) return null;
 
+  const handleIngredientChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSearchingFor(e.target.value);
+  };
+
   return (
-    <select className="w-full bg-zinc-700 rounded-[50vw] border-[1px] border-solid border-white/50 form-input ps-4 py-[2.5px]">
+    <select
+      className="w-full bg-zinc-700 rounded-[50vw] border-[1px] border-solid border-white/50 form-input ps-4 py-[2.5px]"
+      onChange={handleIngredientChange}
+    >
       {data.drinks
         .sort((a: any, b: any) => {
           return a.strIngredient1.localeCompare(b.strIngredient1);
         })
         .map((ingredient: any, i: any) => {
           return (
-            <option
-              value={ingredient.strIngredient1}
-              key={i}
-              onClick={(e) => setSearchingFor(e.target.value)}
-            >
+            <option value={ingredient.strIngredient1} key={i}>
               {ingredient.strIngredient1}
             </option>
           );
